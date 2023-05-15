40c2507f324dc25d19b767049daa0b466df4793d
Ezz <ezmarie97@gmail.com>
2023-05-15 19:14:03 +0300

    added filter options to the cards page

--- client/src/pages/associationsCards.js
+++ client/src/pages/associationsCards.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Space, Card, Button, Pagination, Avatar, Modal } from "antd";
+import { Space, Card, Button, Pagination, Avatar, Modal, message } from "antd";
 import nazareth from "../images/NAZARETH_LOGO3.jpg";
 import boy from "../images/boyAvatar.png";
 import girl from "../images/girlAvatar.png";
@@ -48,6 +48,11 @@ const Cards = () => {
   const [activities, setActivities] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
 
+  const [searchTerm, setSearchTerm] = useState(""); // New state for the search term
+  const [locationTerm, setLocationTerm] = useState("");
+  const [datePosted, setDatePosted] = useState("");
+
+
   // useEffect(() => {
   //   // Fetch activities from the server and update the activities state
   //   fetch("http://localhost:3001/GetActivities")
@@ -55,24 +60,68 @@ const Cards = () => {
   //     .then((data) => setActivities(data));
   // }, []);
 
+  // useEffect(() => {
+  //   // Fetch activities from the server and update the activities state
+  //   fetch("http://localhost:3001/getActivities")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const formattedData = data.map((activity) => ({
+  //         ...activity,
+  //         activityDate: new Date(activity.activityDate),
+  //       }));
+  //       setActivities(formattedData);
+  //       console.log("got Activites = ", activities);
+  //     });
+  // }, []);
+
   useEffect(() => {
-    // Fetch activities from the server and update the activities state
-    fetch("http://localhost:3001/getActivities")
-      .then((response) => response.json())
-      .then((data) => {
+    const fetchActivities = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/getActivities");
+        const data = await response.json();
         const formattedData = data.map((activity) => ({
           ...activity,
           activityDate: new Date(activity.activityDate),
         }));
         setActivities(formattedData);
         console.log("got Activites = ", activities);
-      });
+      } catch (error) {
+        console.error("Failed to fetch activities:", error);
+      }
+    };
+
+    fetchActivities();
   }, []);
+
+  const filteredActivities = activities.filter((activity) => {
+    const now = new Date();
+    let dateLimit;
+    switch (datePosted) {
+      case '24_hours':
+        dateLimit = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1);
+        break;
+      case 'past_week':
+        dateLimit = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7);
+        break;
+      case 'past_month':
+        dateLimit = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+        break;
+      default:
+        dateLimit = new Date(0); // a date far in the past
+    }
+  
+    return (
+      activity.activityName.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      activity.associationAddress.toLowerCase().includes(locationTerm.toLowerCase()) &&
+      activity.activityDate > dateLimit
+    );
+  });
+  
 
   const startIndex = (currentPage - 1) * 20;
   const endIndex = startIndex + 20;
 
-  activities.slice(startIndex, endIndex).forEach((activity, index) => {
+  filteredActivities.slice(startIndex, endIndex).forEach((activity, index) => {
     cardsGrid.push(
       <Card.Grid
         key={`content-${index}`}
@@ -116,16 +165,25 @@ const Cards = () => {
 
   const joinActivity = async (activityId, signedUser) => {
     console.log("Activity: " + activityId);
-    signedUser = JSON.parse(localStorage.getItem('user'));
-    console.log("User: " + signedUser._id, ' Token: ', localStorage.getItem('token'));
+    signedUser = JSON.parse(localStorage.getItem("user"));
+    console.log(
+      "User: " + signedUser._id,
+      " Token: ",
+      localStorage.getItem("token")
+    );
     try {
-      const response = await axios.post("http://localhost:3001/joinActivity", {signedUser, activityId},  {
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        },
-      });
-  
+      const response = await axios.post(
+        "http://localhost:3001/joinActivity",
+        { signedUser, activityId },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
       if (response.status === 200) {
+        message.success("Activity Added Successfully");
         console.log(response.data);
       } else {
         console.warn("Bad response ", response);
@@ -135,9 +193,37 @@ const Cards = () => {
       console.error("Error joining activity:", error.message);
     }
   };
-  
+
   return (
     <Space style={{ width: "100vw", height: "100vh" }} direction="vertical">
+<div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
+  <input
+    type="text"
+    placeholder="Search by activity name, association or speciality..."
+    value={searchTerm}
+    onChange={(e) => setSearchTerm(e.target.value)}
+    style={{ flex: '1', marginRight: '10px' }} // added some style for flex and margin
+  />
+  <input
+    type="text"
+    placeholder="Search by city, neighborhood or street..."
+    value={locationTerm}
+    onChange={(e) => setLocationTerm(e.target.value)}
+    style={{ flex: '1', marginRight: '10px' }} // added some style for flex and margin
+  />
+  <select
+    value={datePosted}
+    onChange={(e) => setDatePosted(e.target.value)}
+    style={{ flex: '1' }} // added some style for flex
+  >
+    <option value="">Anytime</option>
+    <option value="24_hours">Past 24 hours</option>
+    <option value="past_week">Past Week</option>
+    <option value="past_month">Past Month</option>
+  </select>
+</div>
+
+
       <Card
         style={{
           height: "50vh",
@@ -157,7 +243,7 @@ const Cards = () => {
           {t("Buttons.back")}
         </Button>
         <Pagination
-          total={activities.length}
+          total={filteredActivities.length}
           showTotal={(total, range) =>
             `${range[0]}-${range[1]} of ${total} items`
           }
